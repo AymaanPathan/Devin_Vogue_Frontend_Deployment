7fe34428937b87c44d4d15009480b011367bed8f
Aymaan <aymaanpathan5@gmail.com>
2024-11-03 13:06:28 +0530

    add sidebar

--- src/Components/AdminPanel/PieChart.jsx
+++ src/Components/AdminPanel/PieChart.jsx
@@ -1,12 +1,19 @@
 /* eslint-disable react/prop-types */
 import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
 import { Pie } from "react-chartjs-2";
-import Sidebar from "./Sidebar.jsx";
+import { Link } from "react-router-dom";
+import { useState } from "react";
 
 // Register the components you need
 ChartJS.register(ArcElement, Tooltip, Legend);
 
 const PieChart = ({ chartData }) => {
+  const [isOpen, setIsOpen] = useState(false);
+  const [selectGraphs, setSelectGraphs] = useState("");
+
+  const handleDropDown = () => {
+    setIsOpen(!isOpen);
+  };
   const options = {
     plugins: {
       legend: {
@@ -17,7 +24,67 @@ const PieChart = ({ chartData }) => {
 
   return (
     <div className="flex flex-col md:flex-row h-screen">
-      <Sidebar />
+      <div className=" border text-black flex flex-col md:flex-shrink-0 h-64 md:h-auto md:w-64 overflow-y-auto">
+        {/* Set height for mobile and overflow */}
+        <div className="p-4">
+          <h2 className="text-xl text-center font-semibold">Admin Panel</h2>
+          <ul className="mt-4">
+            <li className="py-2 text-center hover:bg-gray-200 cursor-pointer">
+              <Link to="/admin/addProduct">Add Product</Link>
+            </li>
+            <li className="py-2 text-center hover:bg-gray-200 cursor-pointer">
+              <Link to="/admin/products">Products List</Link>
+            </li>
+            <li className="py-2 text-center hover:bg-gray-200 cursor-pointer">
+              <Link to="/admin/users">Users Setting</Link>
+            </li>
+            <li
+              onClick={handleDropDown}
+              className="py-2 flex items-center justify-center gap-4 text-center hover:bg-gray-200 cursor-pointer"
+            >
+              <i className={`fa-solid fa-caret-${isOpen ? "down" : "up"}`}></i>
+              <p>Graphs</p>
+            </li>
+            <div
+              className={`transition-all duration-300 ease-in-out ${
+                isOpen ? "max-h-[500px] opacity-100" : "max-h-0 opacity-0"
+              } overflow-hidden`}
+            >
+              <Link to="/graphs/bar">
+                <li
+                  onClick={() => setSelectGraphs("bar")}
+                  className={`py-2 ${
+                    selectGraphs === "bar" ? "bg-white text-black" : ""
+                  } text-center hover:bg-gray-200 cursor-pointer`}
+                >
+                  Bar
+                </li>
+              </Link>
+              <Link to="/graphs/pie">
+                <li
+                  onClick={() => setSelectGraphs("pie")}
+                  className={`py-2 ${
+                    selectGraphs === "pie" ? "bg-white text-black" : ""
+                  } text-center hover:bg-gray-200 cursor-pointer`}
+                >
+                  Pie
+                </li>
+              </Link>
+              <Link to="/graphs/line">
+                <li
+                  onClick={() => setSelectGraphs("line")}
+                  className={`py-2 ${
+                    selectGraphs === "line" ? "bg-white text-black" : ""
+                  } text-center hover:bg-gray-200 cursor-pointer`}
+                >
+                  Line
+                </li>
+              </Link>
+            </div>
+          </ul>
+        </div>
+      </div>
+
       <div className="flex-1 flex items-center justify-center p-4 overflow-y-auto">
         <div className="w-full h-64 md:h-72 lg:h-3/4">
           <Pie data={chartData} options={options} />
--- src/Components/AdminPanel/ProductList.jsx
+++ src/Components/AdminPanel/ProductList.jsx
@@ -1,14 +1,79 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { Productcontext } from "../../Context/Product";
 import { Link } from "react-router-dom";
-import Sidebar from "./Sidebar.jsx";
 
 function ProductList() {
+  const [isOpen, setIsOpen] = useState(false);
+  const [selectGraphs, setSelectGraphs] = useState("");
+
+  const handleDropDown = () => {
+    setIsOpen(!isOpen);
+  };
   const { products, deleteProduct } = useContext(Productcontext);
 
   return (
     <div className="flex flex-col md:flex-row">
-      <Sidebar />
+      <div className=" border text-black flex flex-col md:flex-shrink-0 h-64 md:h-auto md:w-64 overflow-y-auto">
+        {/* Set height for mobile and overflow */}
+        <div className="p-4">
+          <h2 className="text-xl text-center font-semibold">Admin Panel</h2>
+          <ul className="mt-4">
+            <li className="py-2 text-center hover:bg-gray-200 cursor-pointer">
+              <Link to="/admin/addProduct">Add Product</Link>
+            </li>
+            <li className="py-2 text-center hover:bg-gray-200 cursor-pointer">
+              <Link to="/admin/products">Products List</Link>
+            </li>
+            <li className="py-2 text-center hover:bg-gray-200 cursor-pointer">
+              <Link to="/admin/users">Users Setting</Link>
+            </li>
+            <li
+              onClick={handleDropDown}
+              className="py-2 flex items-center justify-center gap-4 text-center hover:bg-gray-200 cursor-pointer"
+            >
+              <i className={`fa-solid fa-caret-${isOpen ? "down" : "up"}`}></i>
+              <p>Graphs</p>
+            </li>
+            <div
+              className={`transition-all duration-300 ease-in-out ${
+                isOpen ? "max-h-[500px] opacity-100" : "max-h-0 opacity-0"
+              } overflow-hidden`}
+            >
+              <Link to="/graphs/bar">
+                <li
+                  onClick={() => setSelectGraphs("bar")}
+                  className={`py-2 ${
+                    selectGraphs === "bar" ? "bg-white text-black" : ""
+                  } text-center hover:bg-gray-200 cursor-pointer`}
+                >
+                  Bar
+                </li>
+              </Link>
+              <Link to="/graphs/pie">
+                <li
+                  onClick={() => setSelectGraphs("pie")}
+                  className={`py-2 ${
+                    selectGraphs === "pie" ? "bg-white text-black" : ""
+                  } text-center hover:bg-gray-200 cursor-pointer`}
+                >
+                  Pie
+                </li>
+              </Link>
+              <Link to="/graphs/line">
+                <li
+                  onClick={() => setSelectGraphs("line")}
+                  className={`py-2 ${
+                    selectGraphs === "line" ? "bg-white text-black" : ""
+                  } text-center hover:bg-gray-200 cursor-pointer`}
+                >
+                  Line
+                </li>
+              </Link>
+            </div>
+          </ul>
+        </div>
+      </div>
+
       <div className="p-4 flex-1">
         <h2 className="text-3xl font-semibold mb-8 text-center text-indigo-600">
           Products List
